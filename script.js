d161ca0235caa453ca3e4bde1bff303d9ecaa309
alifkoperbanjarbaru <nofianur27@gmail.com>
2026-07-14 21:52:10 +0800

    Tambah foto produk

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const produk = [
     {
-        nama: "Contoh Koper",
+        nama: "Koper Contoh",
         ukuran: "20 inch",
         warna: "Hitam",
         harga: "Rp290.000",
-        status: "Stok"
+        status: "Stok",
+        foto: "https://drive.google.com/uc?export=view&id=1a97DY1_pI-jhnWAV9TlkNkdnpSrFqRF8"
     },
     {
-        nama: "Contoh Koper 2",
+        nama: "Koper Contoh 2",
         ukuran: "24 inch",
         warna: "Biru",
         harga: "Rp350.000",
-        status: "Stok"
+        status: "Stok",
+        foto: "https://drive.google.com/uc?export=view&id=1a97DY1_pI-jhnWAV9TlkNkdnpSrFqRF8"
     }
 ];
 
@@ -22,6 +24,7 @@ produk.forEach(item => {
 
     tampilan += `
     <div class="kartu">
+        <img src="${item.foto}">
         <h3>${item.nama}</h3>
         <p>Ukuran: ${item.ukuran}</p>
         <p>Warna: ${item.warna}</p>
