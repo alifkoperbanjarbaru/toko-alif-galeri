1f60f99c5267309a76e4400b05e8b2deb8de7aa8
alifkoperbanjarbaru <nofianur27@gmail.com>
2026-07-14 22:01:55 +0800

    Perbaiki script katalog foto

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const produk = [
     {
         nama: "Koper Contoh",
+        merek: "Polo Team",
         ukuran: "20 inch",
         warna: "Hitam",
         harga: "Rp290.000",
         status: "Stok",
         foto: "https://drive.google.com/uc?export=view&id=1a97DY1_pI-jhnWAV9TlkNkdnpSrFqRF8"
     },
+
     {
         nama: "Koper Contoh 2",
+        merek: "Polo Team",
         ukuran: "24 inch",
         warna: "Biru",
         harga: "Rp350.000",
@@ -20,16 +23,26 @@ const produk = [
 
 let tampilan = "";
 
+
 produk.forEach(item => {
 
     tampilan += `
     <div class="kartu">
-        <img src="${item.foto}">
+
+        <img src="${item.foto}" alt="${item.nama}">
+
         <h3>${item.nama}</h3>
+
+        <p>Merek: ${item.merek}</p>
+
         <p>Ukuran: ${item.ukuran}</p>
+
         <p>Warna: ${item.warna}</p>
+
         <p class="harga">${item.harga}</p>
+
         <p>Status: ${item.status}</p>
+
     </div>
     `;
 
